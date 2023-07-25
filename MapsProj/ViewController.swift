fba5249b3a4a283b5bb0b844ad4141b248ba6879
androidc <chizztectep@gmail.com>
2023-07-25 16:55:27 +0500

    initial

---
+++ MapsProj/ViewController.swift
@@ -0,0 +1,14 @@
+//Created by chizztectep on 25.07.2023 
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
